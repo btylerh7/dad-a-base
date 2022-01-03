c3b47c69b523cbb5964d651294eb7f9e2fd1b095
robustjumprope <91493312+robustjumprope@users.noreply.github.com>
2022-01-02 21:20:47 -0500

    set up gh-pages

---
+++ gh-pages.js
@@ -0,0 +1,15 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/btylerh7/dad-a-base.git', // Update to point to your repository  
+        user: {
+            name: 'btylerh7', // update to use your name
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
